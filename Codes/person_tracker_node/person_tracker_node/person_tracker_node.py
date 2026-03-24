63f827a2af80485cfa96b81b1e36cabd70339b0b
장민규 <mingyu.jang.dev@gmail.com>
2026-03-24 20:49:56 +0900

    fix. 사람 탐지 노드 로직 수정
    사람 탐지 노드가 mediapipe 대신 YOLOv11n-Pose를 사용하도록 변경했습니다.
    사람 탐지 노드가 화면에서 최대 5명까지 탐지하도록 변경했습니다.

--- Codes/person_tracker_node/person_tracker_node/person_tracker_node.py
+++ Codes/person_tracker_node/person_tracker_node/person_tracker_node.py
@@ -1,110 +1,108 @@
 import rclpy
 from rclpy.node import Node
 from sensor_msgs.msg import CompressedImage
-from geometry_msgs.msg import Point
+from custom_interfaces.msg import People, Person
 import cv2
 import numpy as np
-import mediapipe as mp
+from ultralytics import YOLO
 
 class PersonTrackerNode(Node):
     def __init__(self):
-        super().__init__('person_tracker_node')
-        model_path = "/home/ubuntu/project_ws/src/person_tracker_node/person_tracker_node/pose_landmarker_lite.task"
-        
-        # 1. ROS 2 Topic 설정
-        # /image_out/compressed 토픽 구독
-        self.subscription = self.create_subscription(
-            CompressedImage, 
-            '/image_out/compressed', 
-            self.image_callback, 
-            10)
-        
-        # 사람 중심점 발행 (Point 메시지)
-        self.publisher = self.create_publisher(Point, '/point_out', 10)
+      super().__init__('person_tracker_node')
+      
+      # Image topic subscription
+      self.subscription = self.create_subscription(
+          CompressedImage, 
+          '/image_out/compressed', 
+          self.image_callback, 
+          10)
+      
+      # People points publisher
+      self.publisher = self.create_publisher(People, '/point_out', 10)
 
-        # 2. MediaPipe Pose 설정
-        try:
-          BaseOptions = mp.tasks.BaseOptions
-          self.PoseLandmarker = mp.tasks.vision.PoseLandmarker
-          self.options = mp.tasks.vision.PoseLandmarkerOptions(
-            base_options=BaseOptions(model_asset_path=model_path),
-            running_mode=mp.tasks.vision.RunningMode.IMAGE)
-          self.landmarker = self.PoseLandmarker.create_from_options(self.options)
-          self.get_logger().info("MediaPipe Landmarker initialized.")
-        except Exception as e:
-          self.get_logger().error(f"Failed to initialize: {str(e)}")
-          raise e
+      # YOLO model define
+      model_path = "/home/ubuntu/project_ws/src/person_tracker_node/person_tracker_node/yolo11n-pose.pt"
+      self.model = YOLO(model_path)
+      
+      # CLAHE filter initalize
+      self.clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
 
-        # 3. CLAHE 객체 생성 (대비 제한 2.0, 그리드 8x8)
-        self.clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
-
-        self.get_logger().info('Person Tracker Node has been started (Depth: Right Side)')
+      self.get_logger().info('Person Tracker Node has been started')
 
     def image_callback(self, msg):
-        # A. 압축 이미지 디코딩
-        np_arr = np.frombuffer(msg.data, np.uint8)
-        frame = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+      # JPEG decode
+      np_arr = np.frombuffer(msg.data, np.uint8)
+      frame = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
 
-        if frame is None:
-            return
+      if frame is None:
+          return
 
-        # B. Depth 카메라 부분 추출 (오른쪽 절반)
-        h, w, _ = frame.shape
-        # [세로 전체, 가로 중간부터 끝까지]
-        depth_roi = frame[:, w//2:] 
+      # Depth Image(right half)
+      h, w, _ = frame.shape
+      depth_roi = frame[:, w//2:] 
 
-        # C. Median Filter (3x3) - 노이즈 제거
-        filtered = cv2.medianBlur(depth_roi, 3)
+      # Apply median filter
+      filtered = cv2.medianBlur(depth_roi, 3)
 
-        # D. CLAHE 적용 (Y채널만)
-        # YUV 색공간으로 변환하여 밝기(Y) 채널만 선명하게 만듦
-        yuv = cv2.cvtColor(filtered, cv2.COLOR_BGR2YUV)
-        yuv[:, :, 0] = self.clahe.apply(yuv[:, :, 0])
-        processed_img = cv2.cvtColor(yuv, cv2.COLOR_YUV2BGR)
+      # Y Channel CLAHE filtering
+      yuv = cv2.cvtColor(filtered, cv2.COLOR_BGR2YUV)
+      yuv[:, :, 0] = self.clahe.apply(yuv[:, :, 0])
+      processed_img = cv2.cvtColor(yuv, cv2.COLOR_YUV2BGR)
 
-        # E. MediaPipe 추론 (RGB 이미지 입력 필요)
-        rgb_img = cv2.cvtColor(processed_img, cv2.COLOR_BGR2RGB)
-        mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_img)
-        results = self.landmarker.detect(mp_image)
-        point_msg = Point()
-        # F. 11, 12, 23, 24번 랜드마크를 이용한 중심점 계산
-        if results.pose_landmarks:
-            landmarks = results.pose_landmarks[0]
-            
-            # 인덱스 정의: 11(L.Shoulder), 12(R.Shoulder), 23(L.Hip), 24(R.Hip)
-            target_indices = [11, 12, 23, 24]
-            
-            sum_x = 0.0
-            sum_y = 0.0
-            valid_points = 0
-
-            for idx in target_indices:
-                lm = landmarks[idx]
-                # MediaPipe는 가시성(visibility) 점수도 제공합니다.
-                sum_x += lm.x
-                sum_y += lm.y
-                valid_points += 1
-
-            if valid_points > 0:
-                # 정규화된 중심 좌표 (0.0 ~ 1.0)
-                avg_x = sum_x / valid_points
-                avg_y = sum_y / valid_points
-
-                # ROS 2 메시지 발행
-                point_msg.x = avg_x
-                point_msg.y = avg_y
-                point_msg.z = 0.0 # 필요 시 해당 좌표의 실제 거리값 할당
-                
+      # YOLOv11n-Pose detection
+      results = self.model(processed_img, verbose=False)
+      people_msg = People()
+      if len(results) == 0:
+        self.publisher.publish(people_msg)
+        return
+      result = results[0]
+      if result.keypoints is None:
+        self.publisher.publish(people_msg)
+        return
+      
+      # Points of shoulders and hip
+      indices = [5, 6, 11, 12]
+      keypoints = result.keypoints.xyn[:, indices, :].cpu().numpy() # (N, 4, 2)
+      scores = result.keypoints.conf[:, indices].cpu().numpy() # (N, 4)
+      boxes = result.boxes.xyxyn.cpu().numpy() # (N, 4)
+      
+      count = 0
+      for i in range(len(keypoints)):
+        x1, y1, x2, y2 = boxes[i]
+        w_box = x2 - x1
+        h_box = y2 - y1
+        if w_box < 0.05 or h_box < 0.1:
+          continue
+        
+        keypoint = keypoints[i] # (4, 2)
+        conf = scores[i] # (4,)
+        valid_mask = conf > 0.5
+        if np.any(valid_mask):
+          valid_points = keypoint[valid_mask]
+          center = np.mean(valid_points, axis=0)
         else:
-          point_msg.x = -1.0
-          point_msg.y = -1.0
-          point_msg.z = 0.0
-        self.publisher.publish(point_msg)
-    def visualize(self, img, x, y):
-        cv2.circle(img, (x, y), 8, (0, 255, 0), -1)
-        cv2.putText(img, "Center", (x + 10, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-        cv2.imshow("Depth ROI Tracking", img)
-        cv2.waitKey(1)
+          continue
+        
+        if np.sum(valid_mask) == 4:
+          shoulder_center = np.mean(keypoint[:2], axis=0)
+          hip_center = np.mean(keypoint[2:], axis=0)
+          
+          dx, dy = shoulder_center - hip_center
+          angle = abs(np.arctan2(dy, dx))
+          state = 1.0 if angle > 1.0 else 2.0
+        else:
+          state = 0.0
+        
+        person = Person()
+        person.x = float(center[0])
+        person.y = float(center[1])
+        person.state = float(state)
+        
+        people_msg.people.append(person)
+        count += 1
+        if count >= 5:
+          break
+      self.publisher.publish(people_msg)
 
 def main(args=None):
     rclpy.init(args=args)
